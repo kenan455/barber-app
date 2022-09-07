efe007b8a4f8b8afc3d61e0aa0d8f10a17eae940
Kenan Fintelman <kenanfintelman123@hotmail.com>
2022-09-07 15:39:45 -0300

    fix: making the app work

---
+++ src/screens/Appointments/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+import {Container} from './styles';
+
+export default function Appointments() {
+  return (
+    <Container>
+      <Text>Appointments</Text>
+    </Container>
+  );
+}
+//testwqerqwe@gmail.com
